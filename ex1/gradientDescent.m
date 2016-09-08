0f61edd2200dbe939abd5899694f1fe8517a28a0
Anna Patalakha <anna.patalakha@gmail.com>
2016-09-08 12:22:39 +0200

    Ex 1: Gradient descent

--- ex1/gradientDescent.m
+++ ex1/gradientDescent.m
@@ -17,11 +17,11 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
+    x = X(:,2);
+    h = theta(1) + theta(2) * x;
 
-
-
-
-
+    theta(1) = theta(1) - alpha * (1/m) * sum(h - y);
+    theta(2) = theta(2) - alpha * (1/m) * sum((h - y) .* x);
 
     % ============================================================
 
